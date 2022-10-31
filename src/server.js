e34c8aabfae94108f05aa9306e5b5d237f83050c
bsande6 <bsande6@g.clemson.edu>
2022-10-30 21:27:35 -0400

    Adding trivia question

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // use the express library
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const fetch = require('node-fetch');
 
 
 // create a new server application
@@ -45,6 +46,62 @@ app.get('/', (req, res) => {
   });
 });
 
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+app.get("/trivia", async (req, res) => {
+  // fetch the data
+  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+
+  // fail if bad response
+  if (!response.ok) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+    return;
+  }
+
+  // interpret the body as json
+  const content = await response.json();
+
+  // fail if db failed
+  if (content.response_code !== 0) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with internal response code ${data.response_code}`);
+    return;
+  }
+
+  // respond to the browser
+  // TODO: make proper html
+  //res.send(JSON.stringify(content, 2));
+  console.log(content)
+  console.log(content['results'][0]['answers'])
+  //answers=content['results'][0]['incorrect_answers']
+  //answers.push(content['results'][0]['incorrect_answers'])
+  //answers.push(content['results'][0]['correct_answers'])
+  const correctAnswer = content['results'][0]['correct_answer'];
+  let answers = content['results'][0]['incorrect_answers'];
+  answers.push(content['results'][0]['correct_answer'])
+
+  const answerLinks = answers.map(answer => {
+    return `<a href="javascript:alert('${
+      answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+      }')">${answer}</a>
+    `
+  })
+  
+  res.render('trivia', {
+    question: req.query.question || content['results'][0]['question'],
+    answers: req.query.answers || answerLinks,
+    category: req.query.category || content['results'][0]['category'],
+    difficulty: req.query.difficulty || content['results'][0]['difficulty']
+  })
+});
 // Start listening for network connections
 app.listen(port);
 
